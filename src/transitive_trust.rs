5ef0a904ab0c47863d1dc46821315b1f40b46a48
Filip Lazovic <github.grumbling276@passinbox.com>
2024-10-06 18:42:35 +0200

    Core logic of transitive trust

--- src/transitive_trust.rs
+++ src/transitive_trust.rs
@@ -1,80 +1,191 @@
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    vec::IntoIter,
+};
 
-fn trust_policy(weights: &HashMap<(u32, u32), f32>) -> bool {
-    if weights.len() > 5 {
-        return false;
-    }
-    for (_, v) in weights {
-        if *v > 0.8 || *v <= 0.0 {
-            return false;
-        }
-    }
-    return true;
+use priority_queue::PriorityQueue;
+
+struct Result {
+    node: String,
+    p_score: i32,
+    n_score: i32,
 }
 
-fn h(inspected: &HashMap<u32, bool>, scores: &HashMap<u32, f32>) -> HashMap<u32, Vec<u32>> {
-    let mut sets = HashMap::new();
-    for (i, v_i) in scores {
-        let ins_i = inspected.get(&i).unwrap_or(&false);
-        let mut set_i = Vec::new();
-        for (j, v_j) in scores {
-            let ins_j = inspected.get(&i).unwrap_or(&false);
-            if *ins_i && *ins_j && v_i > v_j {
-                set_i.push(*j);
+impl Result {
+    pub fn new(node: String, p_score: i32, n_score: i32) -> Self {
+        Self {
+            node,
+            p_score,
+            n_score,
+        }
+    }
+
+    pub fn net_score(&self) -> i32 {
+        self.p_score - self.n_score
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    positive_edges: HashMap<String, u16>,
+    negative_edges: HashMap<String, u16>,
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Self {
+            positive_edges: HashMap::new(),
+            negative_edges: HashMap::new(),
+        }
+    }
+
+    pub fn add_positive_edge(&mut self, target: String, weight: u16) {
+        self.positive_edges.insert(target, weight);
+    }
+
+    pub fn add_negative_edge(&mut self, target: String, weight: u16) {
+        self.negative_edges.insert(target, weight);
+    }
+
+    pub fn get_positive_weight(&self, target: String) -> u16 {
+        self.positive_edges.get(&target).cloned().unwrap()
+    }
+
+    pub fn get_negative_weight(&self, target: String) -> u16 {
+        self.negative_edges.get(&target).cloned().unwrap()
+    }
+
+    pub fn out_neighbours(&self) -> Vec<String> {
+        let mut positive_keys: HashSet<&String> = self.positive_edges.keys().collect();
+        let negative_keys: HashSet<&String> = self.negative_edges.keys().collect();
+        positive_keys.extend(negative_keys);
+        positive_keys.into_iter().cloned().collect()
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Graph {
+    nodes: HashMap<String, Node>,
+}
+
+impl Graph {
+    pub fn new() -> Self {
+        Self {
+            nodes: HashMap::new(),
+        }
+    }
+
+    pub fn add_positive_edge(&mut self, source: String, target: String, weight: u16) {
+        if !self.nodes.contains_key(&source) {
+            self.nodes.insert(source.clone(), Node::new());
+        }
+        if !self.nodes.contains_key(&target) {
+            self.nodes.insert(target.clone(), Node::new());
+        }
+
+        let node = self.nodes.get_mut(&source).unwrap();
+        node.add_positive_edge(target, weight);
+    }
+
+    pub fn add_negative_edge(&mut self, source: String, target: String, weight: u16) {
+        if !self.nodes.contains_key(&source) {
+            self.nodes.insert(source.clone(), Node::new());
+        }
+        if !self.nodes.contains_key(&target) {
+            self.nodes.insert(target.clone(), Node::new());
+        }
+
+        let node = self.nodes.get_mut(&source).unwrap();
+        node.add_negative_edge(target, weight);
+    }
+
+    pub fn get_positive_weight(&self, source: String, target: String) -> u16 {
+        let node = self.nodes.get(&source).unwrap();
+        node.get_positive_weight(target)
+    }
+
+    pub fn get_negative_weight(&self, source: String, target: String) -> u16 {
+        let node = self.nodes.get(&source).unwrap();
+        node.get_negative_weight(target)
+    }
+
+    pub fn for_each_node(&self) -> IntoIter<String> {
+        let keys: Vec<String> = self.nodes.keys().cloned().collect();
+        keys.into_iter()
+    }
+
+    pub fn for_each_neighbour(&self, node: String) -> IntoIter<String> {
+        let node = self.nodes.get(&node).unwrap();
+        node.out_neighbours().into_iter()
+    }
+}
+
+pub fn compute_scores(graph: Graph, source: String) -> Vec<Result> {
+    let mut p_scores = HashMap::<String, i32>::new();
+    let mut n_scores = HashMap::<String, i32>::new();
+    let mut inspected = HashSet::<String>::new();
+    let mut pq = PriorityQueue::<String, i32>::new();
+
+    for node in graph.for_each_node() {
+        let p_score = if node == source { 1 } else { 0 };
+        p_scores.insert(node.clone(), p_score);
+        n_scores.insert(node.clone(), 0);
+        pq.push(node, p_score);
+    }
+
+    while !pq.is_empty() {
+        let (node_key, _) = pq.pop().unwrap();
+        if inspected.contains(&node_key) {
+            continue;
+        }
+        inspected.insert(node_key.clone());
+
+        let node_score =
+            (p_scores.get(&node_key).unwrap() - n_scores.get(&node_key).unwrap()).max(0);
+
+        for neighbor_key in graph.for_each_neighbour(node_key.clone()) {
+            let neighbor_score =
+                p_scores.get(&neighbor_key).unwrap() - n_scores.get(&neighbor_key).unwrap();
+
+            if inspected.contains(&neighbor_key) || neighbor_score < node_score {
+                continue;
             }
-        }
-        sets.insert(*i, set_i);
-    }
-    sets
-}
 
-fn all_inspected(inspected: &HashMap<u32, bool>) -> bool {
-    for (_, ins) in inspected {
-        if !ins {
-            return false;
-        }
-    }
+            let positive_weight = graph.get_positive_weight(node_key.clone(), neighbor_key.clone());
+            let negative_weight = graph.get_negative_weight(node_key.clone(), neighbor_key.clone());
 
-    return true;
-}
+            let neighbour_p_score = p_scores.get(&neighbor_key).unwrap();
+            let neighbour_n_score = n_scores.get(&neighbor_key).unwrap();
 
-fn run(
-    mut inspected: HashMap<u32, bool>,
-    mut scores: HashMap<u32, f32>,
-    weights: HashMap<(u32, u32), f32>,
-) -> HashMap<u32, f32> {
-    assert!(trust_policy(&weights));
-    while !all_inspected(&inspected) {
-        let sets = h(&inspected, &scores);
-        for (i, set_i) in sets {
-            inspected.insert(i, true); // mark as inspected
-            for j in set_i {
-                if !inspected.get(&i).unwrap_or(&false) {
-                    let weight_ij = weights.get(&(i, j)).unwrap_or(&0.0);
-                    let score_j = scores.get(&j).unwrap_or(&0.0);
-                    let score_i = scores.get(&i).unwrap_or(&0.0);
-                    let new_score_j = score_j + (score_i - score_j) * weight_ij;
-                    scores.insert(j, new_score_j.max(0.0));
-                }
-            }
+            if node_score > *neighbour_p_score {
+                let new_neighbour_p_score = neighbour_p_score
+                    + (node_score - neighbour_p_score) * i32::from(positive_weight);
+                p_scores.insert(neighbor_key.clone(), new_neighbour_p_score);
+            };
+
+            if node_score > *neighbour_n_score {
+                let new_neighbour_n_score = neighbour_n_score
+                    + (node_score - neighbour_n_score) * i32::from(negative_weight);
+                n_scores.insert(neighbor_key.clone(), new_neighbour_n_score);
+            };
+
+            let neighbour_p_score = p_scores.get(&neighbor_key).unwrap();
+            let neighbour_n_score = n_scores.get(&neighbor_key).unwrap();
+            pq.push(neighbor_key, neighbour_p_score - neighbour_n_score);
         }
     }
-    scores
+
+    let mut results = Vec::new();
+    for node in graph.for_each_node() {
+        if node != source {
+            let p_score = p_scores.get(&node).unwrap();
+            let n_score = n_scores.get(&node).unwrap();
+            let result = Result::new(node, *p_score, *n_score);
+            results.push(result);
+        }
+    }
+
+    results
 }
 
-pub fn run_job() {
-    let nodes = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let inspected = nodes.clone().into_iter().map(|x| (x, false)).collect();
-    let mut scores: HashMap<u32, f32> = nodes.into_iter().map(|x| (x, 0.0)).collect();
-    scores.insert(1, 1.0);
-    let mut weights = HashMap::new();
-    weights.insert((1, 2), 0.2);
-    weights.insert((3, 4), 0.6);
-    weights.insert((4, 2), 0.3);
-    weights.insert((6, 4), 0.2);
-    weights.insert((10, 6), 0.6);
-    let final_scores = run(inspected, scores, weights);
-    for (i, score) in final_scores {
-        println!("{}: {}", i, score);
-    }
-}
+pub fn run_job() {}
